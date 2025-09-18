0e0c78036cc78a9fb280c41542ef7f0a8d80a274
Gabriel <tanakaamc1@gmail.com>
2025-09-18 13:24:30 -0300

    Update dateTime

--- src/ticketlog_etl.py
+++ src/ticketlog_etl.py
@@ -106,7 +106,7 @@ def insert_transaction(cursor, transacao, codigo_cliente):
     ))
 
 def fetch_and_save_transactions():
-    start_date = datetime(2025, 9, 1)
+    start_date = datetime(2025, 15, 1)
     end_date = datetime.today()
     current_date = start_date
 
